87a7ee0e5e6d1981b7095331eb4b6bd4e961641d
vinicius <vmm_7@hotmail.com>
2024-09-22 10:02:03 -0300

    First commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+
+/* 
+Definition and Usage
+The <header> element represents a container for introductory content or a set of navigational links.
+
+A <header> element typically contains:
+
+one or more heading elements (<h1> - <h6>)
+logo or icon
+authorship information
+
+https://www.w3schools.com/tags/tag_header.asp
+*/
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
