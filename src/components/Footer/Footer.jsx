7a6e11d17d508a08a4a0df4f537456bcf61f6ac5
vinicius <vmm_7@hotmail.com>
2024-10-09 20:22:43 -0300

    Add: json-server

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Footer</p>
+      <p>Footer &copy; 2024</p>
     </footer>
   );
 }
