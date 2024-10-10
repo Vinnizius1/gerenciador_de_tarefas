1ba68ab58c1d5676a2f657f73940aeb0a61dcdd5
vinicius <vmm_7@hotmail.com>
2024-10-09 22:36:44 -0300

    Refatorando TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -7,38 +7,35 @@ import TaskItem from "../TaskItem/TaskItem";
 import styles from "./TaskList.module.css";
 import TaskFormInput from "../TaskFormInput/TaskFormInput";
 
-/* Componente que receberá tarefas do array no componente "App.jsx" e renderizará cada uma delas como numa lista */
-
 const TaskList = () => {
-  //
   const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null); // Adicionado para capturar erro de rede
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await api.get("/tasks");
+        const response = await api.get("/tasks", { timeout: 5000 }); //
         setTasks(response.data);
       } catch (error) {
         console.error("Error fetching tasks:", error);
-        // Optionally display an error message to the user
+        setError(
+          "Erro ao buscar tarefas. Verifique sua conexão com o servidor."
+        );
       }
     };
     fetchData();
   }, []);
 
-  //
   const handleTaskAdded = newTask => {
-    setTasks([...tasks, newTask]);
+    setTasks(() => [...tasks, newTask]);
   };
 
-  //
   const handleTaskUpdated = updatedTask => {
     setTasks(
       tasks.map(task => (task.id === updatedTask.id ? updatedTask : task))
     );
   };
 
-  //
   const handleTaskDeleted = taskId => {
     setTasks(tasks.filter(task => task.id !== taskId));
   };
@@ -46,15 +43,13 @@ const TaskList = () => {
   return (
     <>
       <h2>Minhas Tarefas</h2>
-
+      {error && <p>{error}</p>} {/* Exibe erro caso ocorra */}
       <TaskFormInput onTaskAdded={handleTaskAdded} />
-
       <ul className={styles.taskList}>
         {tasks.map(task => (
           <li key={task.id}>
             <TaskItem
               task={task}
-              // onToggleTask={onToggleTask}
               onDeleteTask={handleTaskDeleted}
               onEditTask={handleTaskUpdated}
             />
